b3ba431d2ec277f697ed2c69428e3c7843b6f32f
vikash kumar <iit2016041@iiita.ac.in>
2017-12-12 21:45:02 +0530

    my initial file used in submission

---
+++ template.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      cout<<"*"<<endl;
+    }
+    return 0;
+}
