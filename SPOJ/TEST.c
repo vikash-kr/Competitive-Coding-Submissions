37f3ad76aa05ea9b3d63438ef2680e5fbe1e593b
vikash kumar <iit2016041@iiita.ac.in>
2017-12-20 12:18:28 +0530

    Update TEST.c

--- SPOJ/TEST.c
+++ SPOJ/TEST.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 int main(void) {
 	// your code here
-	int a,b,s;
-	scanf("%d", &a);
-	scanf("%d",&b);
-	s=a+b;
-	printf("%d",s);
+    int i = 0;
+    while (scanf("%d\n", &i) > 0 && i != 42) {
+        printf("%d\n", i);
+    }
 
 	return 0;
 }
