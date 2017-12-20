2857409bae2c398832f1370cca3789cb25c4a20c
vikash kumar <iit2016041@iiita.ac.in>
2017-12-20 12:40:28 +0530

    A Game with Numbers

---
+++ SPOJ/NGM.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+ 	if( t%10 !=0){
+  		cout<<"1"<<endl;
+  		cout<<t%10<<endl;
+  	}else
+    cout<<"2"<<endl;
+
+	return 0;
+}
