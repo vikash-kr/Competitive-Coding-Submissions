ebb183cc9b5dc59edc6d718438d04242c640dcad
vikash kumar <iit2016041@iiita.ac.in>
2018-01-08 16:33:45 +0530

    Create STRMRG.cpp

---
+++ CodeChef/STRMRG.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,l1,l2;
+	cin>>t;
+	string s1, s2;
+	while(t--){
+	    cin>>l1>>l2;
+	    cin>>s1;
+	    cin>>s2;
+	    
+	}
+	return 0;
+}
